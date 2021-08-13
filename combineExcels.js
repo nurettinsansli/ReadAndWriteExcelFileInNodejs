b02a9d3f4361a961c7e07d943b22f9e557bce75a
Nurettin Şanslı <nurettin.sansli@hepsiburada.com>
2021-08-13 19:08:43 +0300

    Added: calculate total data count from all the excel

--- combineExcels.js
+++ combineExcels.js
@@ -1,11 +1,15 @@
 var xlsx = require("xlsx");
+var fs = require("fs");
+var path = require("path");
+
+var sourceDir = "Files";
 
 /*
 author: nurettinsanslii@gmail.com
 */
 
 /*
-This function get data from excel. Then converted to json and writes in console
+This function calculate total data count from all the excel.
 */
 
 function readFileToJson(fileName){
@@ -16,6 +20,18 @@ function readFileToJson(fileName){
     return data;
 }
 
-var data = readFileToJson("1.xlsx");
+var tagetDir = path.join(__dirname,sourceDir);
+var files = fs.readdirSync(tagetDir);
+var combinedData = [];
 
-console.log(data);
+files.forEach(function(file){
+    var fileExtenstion = path.parse(file).ext;
+    if(fileExtenstion === ".xlsx" && file[0] !== "~"){
+        var fullFilePath = path.join(__dirname,sourceDir,file);
+        console.log(fullFilePath);
+        var data = readFileToJson(fullFilePath);
+        combinedData = combinedData.concat(data);
+    }
+});
+
+console.log(combinedData.length);
